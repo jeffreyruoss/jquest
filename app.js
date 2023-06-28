3e302122a5925574ed38e48465ea8dcad04a37b4
Jeff Ruoss <jeffreyruoss@gmail.com>
2023-06-27 21:52:22 -0400

    Refactor and fix.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('#user-form');
-  const userNameInput = document.querySelector('#user-name');
-  const questList = document.querySelector('#quest-list');
-  const container = document.querySelector('main.container');
-  const messageDisplay = document.querySelector('#messages');
-
-  const savedData = localStorage.getItem('real-life-quest-app');
-  if (savedData) {
+    // Getting references to DOM elements
+    const form = document.querySelector('#user-form');
+    const userNameInput = document.querySelector('#user-name');
+    const questList = document.querySelector('#quest-list');
+    const completedQuests = document.querySelector('#completed-quests');
+    const container = document.querySelector('main.container');
+    const messageDisplay = document.querySelector('#messages');
+    const fireworkContainers = document.querySelectorAll('.firework-container');
+  
+    // Load quests from localStorage on page load
+    const savedData = localStorage.getItem('real-life-quest-app');
+    if (savedData) {
       const data = JSON.parse(savedData);
       welcomeUser(data.userName);
       data.quests.forEach((quest, index) => {
@@ -17,15 +21,15 @@ document.addEventListener('DOMContentLoaded', () => {
       } else {
           displayMessage(1);
       }
-  } else {
+    } else {
       // Add your quests here only if there is no saved data
       createQuest('First Quest', false, 0);
       createQuest('Second Quest', false, 1);
       //...
       displayMessage(0);
-  }
-
-  form.addEventListener('submit', (e) => {
+    }
+  
+    form.addEventListener('submit', (e) => {
       e.preventDefault();
       welcomeUser(userNameInput.value);
       localStorage.setItem('real-life-quest-app', JSON.stringify({
@@ -33,121 +37,77 @@ document.addEventListener('DOMContentLoaded', () => {
           quests: getQuests()
       }));
       displayMessage(1);
-  });
-
-  function welcomeUser(userName) {
-      form.style.display = 'none';
-  }
-
-  function displayMessage(index) {
-      messageDisplay.textContent = messages[index];
-  }
-
-  function createQuest(title, completed = false, index) {
+    });
+  
+    questList.addEventListener('click', (e) => {
+      if (e.target.tagName === 'INPUT') {
+          const checkBox = e.target;
+          const questItem = checkBox.parentElement;
+          const questTitle = questItem.querySelector('span');
+  
+          addFireworks();
+  
+          if (checkBox.checked) {
+            questTitle.classList.add('completed');
+            completedQuests.appendChild(questItem);
+          } else {
+            questTitle.classList.remove('completed');
+            questList.appendChild(questItem);
+          }
+  
+          const quests = JSON.parse(localStorage.getItem('real-life-quest-app'));
+          const quest = quests.quests.find(quest => quest.title === questTitle.textContent);
+          quest.completed = checkBox.checked;
+          localStorage.setItem('real-life-quest-app', JSON.stringify(quests));
+      }
+    });
+  
+    function welcomeUser(userName) {
+        form.style.display = 'none';
+    }
+  
+    function displayMessage(index) {
+        messageDisplay.textContent = messages[index];
+    }
+  
+    function createQuest(title, completed = false, index) {
       const questItem = document.createElement('div');
       const checkBox = document.createElement('input');
       checkBox.type = 'checkbox';
       checkBox.checked = completed;
-      checkBox.addEventListener('change', () => {
-          localStorage.setItem('real-life-quest-app', JSON.stringify({
-              userName: userNameInput.value,
-              quests: getQuests()
-          }));
-          if (index === 0 && checkBox.checked) {
-              displayMessage(2);
-          }
-      });
       const questTitle = document.createElement('span');
       questTitle.textContent = title;
       questItem.appendChild(checkBox);
       questItem.appendChild(questTitle);
-      questList.appendChild(questItem);
-  }
-
-  function getQuests() {
-      return Array.from(questList.children).map(child => ({
+      completed ? completedQuests.appendChild(questItem) : questList.appendChild(questItem);
+    }
+  
+    function getQuests() {
+      const allQuests = Array.from(questList.children).concat(Array.from(completedQuests.children));
+      return allQuests.map(child => ({
           title: child.querySelector('span').textContent,
           completed: child.querySelector('input').checked
       }));
-  }
-});
-
-const addDeleteLocalStorageButton = () => {
-  const deleteLocalStorageButton = document.createElement('button');
-  deleteLocalStorageButton.id = 'delete-local-storage';
-  deleteLocalStorageButton.textContent = 'Delete Local Storage';
-  document.body.prepend(deleteLocalStorageButton);
-};
-
-const deleteLocalStorage = () => {
-  localStorage.clear();
-  window.location.reload();
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-  if (window.location.hostname === 'localhost') {
-    addDeleteLocalStorageButton();
-
-    document
-      .getElementById('delete-local-storage')
-      .addEventListener('click', deleteLocalStorage);
-  }
-});
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const fireworkContainers = document.querySelectorAll('.firework-container');
-    const questList = document.querySelector('#quest-list');
-    questList.addEventListener('click', addFireworksOnClick(fireworkContainers));
-});
-
-const addFireworksOnClick = (fireworkContainers) => {
-    return (e) => {
-        if (e.target.tagName === 'INPUT') {
-            fireworkContainers.forEach(container => container.classList.add('firework'));
-            setTimeout(() => {
-                fireworkContainers.forEach(container => container.classList.remove('firework'));
-            }
-            , 1500);
-        }
-    };
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-    const questList = document.querySelector('#quest-list');
-    const completedQuests = document.querySelector('#completed-quests');
-    questList.addEventListener('click', moveCompletedQuest(completedQuests));
-});
-
-const moveCompletedQuest = (completedQuests) => {
-    return (e) => {
-        if (e.target.tagName === 'INPUT') {
-            const questItem = e.target.parentElement;
-            const questTitle = questItem.querySelector('span');
-            questTitle.classList.add('completed');
-            completedQuests.appendChild(questTitle);
-            questItem.remove();
-        }
-    };
-}
-
-
-const questList = document.querySelector('#quest-list');
-questList.addEventListener('click', toggleQuestCompletion);
-
-function toggleQuestCompletion(e) {
-    if (e.target.tagName === 'INPUT') {
-        const questItem = e.target.parentElement;
-        const questTitle = questItem.querySelector('span');
-        const quests = JSON.parse(localStorage.getItem('real-life-quest-app'));
-        const quest = quests.quests.find(quest => quest.title === questTitle.textContent);
-        quest.completed = !quest.completed;
-        localStorage.setItem('real-life-quest-app', JSON.stringify(quests));
     }
-}
-
-
-
-
-
-
+  
+    function addFireworks() {
+      fireworkContainers.forEach(container => container.classList.add('firework'));
+      setTimeout(() => {
+          fireworkContainers.forEach(container => container.classList.remove('firework'));
+      }, 1500);
+    }
+  
+    // Local Storage Delete Button for Localhost Testing
+    if (window.location.hostname === 'localhost') {
+      const deleteLocalStorageButton = document.createElement('button');
+      deleteLocalStorageButton.id = 'delete-local-storage';
+      deleteLocalStorageButton.textContent = 'Delete Local Storage';
+      document.body.prepend(deleteLocalStorageButton);
+    
+      deleteLocalStorageButton.addEventListener('click', () => {
+        localStorage.clear();
+        window.location.reload();
+      });
+    }
+  });
+  
